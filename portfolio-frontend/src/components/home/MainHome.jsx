43dd493bba8bdafa4b632a15d91cea9063f13233
Judy Odiakose <97225272+JudyTrillz@users.noreply.github.com>
2025-03-12 02:05:17 +0100

    Finished the home section and made it responsive
    The home section is subject to image change

--- portfolio-frontend/src/components/home/MainHome.jsx
+++ portfolio-frontend/src/components/home/MainHome.jsx
@@ -1,9 +1,89 @@
-import { Element } from "react-scroll";
+import "./MainHome.scss";
+import { Element, Link } from "react-scroll";
+import homeImg from "../../img/banners/home-banner-alt.svg";
+
+import { BiRightArrowAlt } from "react-icons/bi";
+import { RiTwitterXLine } from "react-icons/ri";
+import { RiFacebookCircleFill } from "react-icons/ri";
+import { RiInstagramFill } from "react-icons/ri";
+import { RiLinkedinFill } from "react-icons/ri";
+import { RiGithubFill } from "react-icons/ri";
+
+const twi = <RiTwitterXLine key={"twitter"} />;
+const fb = <RiFacebookCircleFill key={"facebook"} />;
+const ig = <RiInstagramFill key={"instagram"} />;
+const li = <RiLinkedinFill key={"linkedin"} />;
+const gh = <RiGithubFill key={"github"} />;
 
 const MainHome = () => {
+  const icons = [
+    { icon: twi, link: "https://x.com/offixial_judy" },
+    { icon: fb, link: "https://www.facebook.com/juddy.trillz" },
+    { icon: ig, link: "https://www.instagram.com/offixial_judy/" },
+    { icon: li, link: "https://www.linkedin.com/in/judy-odiakose/" },
+    { icon: gh, link: "https://github.com/JudyTrillz" },
+  ];
+
+  const DisplayIcons = () => {
+    return (
+      <div className="home__container--data__socials">
+        {icons.map((icon, index) => (
+          <a
+            href={icon.link}
+            key={index}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="home__container--data__socials__icon">
+            {icon.icon}
+          </a>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <Element name="Home">
-      <section className="section">Home</section>
+      <section className="section home">
+        <div className="home__container">
+          <div className="home__container--data">
+            <div className="home__container--data__holder">
+              <h2 className="home__container--data__tag"> Hi, I&apos;m Judy</h2>
+            </div>
+
+            <h1 className="home__container--data__title">
+              <em className="home__container--data__title--span">A Developer</em> <br />{" "}
+              With a Difference.
+            </h1>
+
+            <p className="home__container--data__desc ">
+              Building visually consistent and high-performance websites. Delivering
+              smooth, Scalable, and accessible digital products with Impact, enhancing
+              user engagement with smooth and intuitive user interfaces.
+            </p>
+
+            <div className="home__container--data__socials home__container--data__holder">
+              <DisplayIcons />
+            </div>
+
+            <div className="home__container--data__btn primary home__container--data__holder">
+              <Link to="Portfolio" className="home__container--data__btn-primary">
+                Explore Projects
+              </Link>
+
+              <Link to="Contact" className="home__container--data__btn-secondary">
+                Hire Me
+                <BiRightArrowAlt />
+              </Link>
+            </div>
+          </div>
+
+          <div className="home__container--img">
+            <figure>
+              <img src={homeImg} alt="home-banner" />
+            </figure>
+          </div>
+        </div>
+      </section>
     </Element>
   );
 };
