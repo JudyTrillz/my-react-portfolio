bb457032690cf9e410bbd6f62444e2cf284f918a
Judy Odiakose <97225272+JudyTrillz@users.noreply.github.com>
2025-03-30 22:35:08 +0100

    Finished the skills section
    It is also subject to change

--- portfolio-frontend/src/components/skill/Skill.jsx
+++ portfolio-frontend/src/components/skill/Skill.jsx
@@ -1,10 +1,55 @@
 import { Element } from "react-scroll";
 import "./Skill.scss";
 
+import {
+  bootstrap,
+  html,
+  css,
+  scss,
+  js,
+  jest,
+  tailwind,
+  sanity,
+  react,
+  next,
+  ts,
+  github,
+} from "../../img/logos";
+
 const Skill = () => {
+  const skillIcons = [
+    { icon: html, name: "HTML" },
+    { icon: css, name: "CSS 3" },
+    { icon: scss, name: "SCSS" },
+    { icon: js, name: "JS" },
+    { icon: react, name: "REACT JS" },
+    { icon: bootstrap, name: "BOOTSTRAP" },
+    { icon: tailwind, name: "TAILWIND" },
+    { icon: next, name: "NEXT JS" },
+    { icon: ts, name: "TYPESCRIPT" },
+    { icon: sanity, name: "SANITY" },
+    { icon: github, name: "GIT-HUB" },
+    { icon: jest, name: "JEST" },
+  ];
+
   return (
     <Element name="Skills">
-      <section className="section container">Skill</section>
+      <section className="section container skills">
+        <h2 className="section-title">Skills</h2>
+
+        <p className="section-intro">
+          Here are the tools and frameworks I specialize in for building modern, scalable
+          web applications.
+        </p>
+
+        <div className="skills__wrapper">
+          {skillIcons.map((icon, index) => (
+            <div className="skills__wrapper--img" key={index} title={icon.name}>
+              <img src={icon.icon} alt="skills icons" loading="lazy" />
+            </div>
+          ))}
+        </div>
+      </section>
     </Element>
   );
 };
---
+++ portfolio-frontend/src/img/logos/index.js
@@ -0,0 +1,12 @@
+export { default as bootstrap } from "./bootstrap-5-1.svg";
+export { default as css } from "./css-3.svg";
+export { default as github } from "./github-icon-1.svg";
+export { default as html } from "./html-1.svg";
+export { default as js } from "./javascript-1.svg";
+export { default as jest } from "./jest-2.svg";
+export { default as next } from "./next-js.svg";
+export { default as react } from "./react-2.svg";
+export { default as sanity } from "./sanity.svg";
+export { default as scss } from "./sass-1.svg";
+export { default as tailwind } from "./tailwind-css-2.svg";
+export { default as ts } from "./typescript.svg";
